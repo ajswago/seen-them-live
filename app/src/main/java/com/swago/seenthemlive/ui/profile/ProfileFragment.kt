9c8636cc5966248bcebba6f981fe5c7b388837e7
Anthony <ajswago@gmail.com>
2020-06-04 21:40:20 -0400

    Add initial content to profile

--- app/src/main/java/com/swago/seenthemlive/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/swago/seenthemlive/ui/profile/ProfileFragment.kt
@@ -1,19 +1,19 @@
 package com.swago.seenthemlive.ui.profile
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.widget.ContentLoadingProgressBar
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.firebase.firestore.FirebaseFirestore
+import com.swago.seenthemlive.LoginActivity
 import com.swago.seenthemlive.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -24,17 +24,10 @@ class ProfileFragment : Fragment() {
 
     private lateinit var profileViewModel: ProfileViewModel
 
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var loading: ContentLoadingProgressBar? = null
+    private var content: ConstraintLayout? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var userId: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,31 +35,78 @@ class ProfileFragment : Fragment() {
     ): View? {
         profileViewModel =
             ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_profile, container, false)
-        val textView: TextView = root.findViewById(R.id.text_share)
-        profileViewModel.text.observe(this, Observer {
-            textView.text = it
+        val profileDisplayName: TextView = root.findViewById(R.id.profile_display_name)
+        val profileUsername: TextView = root.findViewById(R.id.profile_username)
+        val profileEmail: TextView = root.findViewById(R.id.profile_email)
+        val profileConcertCount: TextView = root.findViewById(R.id.profile_num_shows_label)
+        val profileBandsCount: TextView = root.findViewById(R.id.profile_num_bands_label)
+        val profileVenuesCount: TextView = root.findViewById(R.id.profile_num_venues_label)
+        content = root.findViewById(R.id.profile_content)
+        loading = root.findViewById(R.id.loading)
+
+        userId = activity?.intent?.getStringExtra("user")
+
+        profileViewModel.userDisplayName.observe(this, Observer {
+            profileDisplayName.text = it
         })
+        profileViewModel.userUsername.observe(this, Observer {
+            profileUsername.text = it
+        })
+        profileViewModel.userEmail.observe(this, Observer {
+            profileEmail.text = it
+        })
+        profileViewModel.userConcertCount.observe(this, Observer {
+            profileConcertCount.text = "Number of Concerts Attended: ${it}"
+        })
+        profileViewModel.userBandCount.observe(this, Observer {
+            profileBandsCount.text = "Number of Bands Seen: ${it}"
+        })
+        profileViewModel.userVenueCount.observe(this, Observer {
+            profileVenuesCount.text = "Number of Venues Attended: ${it}"
+        })
+
+        updateUi()
+
         return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    fun updateUi() {
+        content?.visibility = View.GONE
+        loading?.show()
+        val db = FirebaseFirestore.getInstance()
+        db.collection("users").document(userId!!)
+            .get()
+            .addOnSuccessListener { documentReference ->
+                val user: LoginActivity.User? = documentReference.toObject(LoginActivity.User::class.java)
+                val concertCount = user?.setlists?.groupBy {
+                    it.eventDate
+                }?.size
+                val bandCount = user?.setlists?.groupBy {
+                    it.artist?.mbid
+                }?.size
+                val venueCount = user?.setlists?.groupBy {
+                    it.venue?.id
+                }?.size
+                val topArtists = user?.setlists?.groupBy { it.artist?.name }?.toList()
+                    ?.sortedByDescending { (_, value) -> value.size }?.take(10)?.toMap()
+                Log.d("Profile", "Top Five Artists: ${topArtists}")
+                topArtists?.forEach { artistMap -> Log.d("Profile", "Artist: ${artistMap.key}, Count: ${artistMap.value.size}") }
+                profileViewModel.userDisplayName.postValue(user?.displayName)
+                profileViewModel.userUsername.postValue(user?.username)
+                profileViewModel.userEmail.postValue(user?.email)
+                profileViewModel.userConcertCount.postValue(concertCount)
+                profileViewModel.userBandCount.postValue(bandCount)
+                profileViewModel.userVenueCount.postValue(venueCount)
+
+                content?.visibility = View.VISIBLE
+                loading?.hide()
+            }
+            .addOnFailureListener { e ->
+                Log.w("CLOUDFIRESTORE", "Error adding document", e)
+                content?.visibility = View.VISIBLE
+                loading?.hide()
             }
     }
 }
--- app/src/main/java/com/swago/seenthemlive/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/swago/seenthemlive/ui/profile/ProfileViewModel.kt
@@ -6,8 +6,33 @@ import androidx.lifecycle.ViewModel
 
 class ProfileViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "Profile Coming Soon!"
+    private val _userDisplayName = MutableLiveData<String>().apply {
+        value = "First Last"
     }
-    val text: LiveData<String> = _text
+    val userDisplayName: MutableLiveData<String> = _userDisplayName
+
+    private val _userUsername = MutableLiveData<String>().apply {
+        value = "usr"
+    }
+    val userUsername: MutableLiveData<String> = _userUsername
+
+    private val _userEmail = MutableLiveData<String>().apply {
+        value = "first.last@email.com"
+    }
+    val userEmail: MutableLiveData<String> = _userEmail
+
+    private val _userConcertCount = MutableLiveData<Int>().apply {
+        value = 0
+    }
+    val userConcertCount: MutableLiveData<Int> = _userConcertCount
+
+    private val _userBandCount = MutableLiveData<Int>().apply {
+        value = 0
+    }
+    val userBandCount: MutableLiveData<Int> = _userBandCount
+
+    private val _userVenueCount = MutableLiveData<Int>().apply {
+        value = 0
+    }
+    val userVenueCount: MutableLiveData<Int> = _userVenueCount
 }
