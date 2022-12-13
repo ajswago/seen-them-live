e0f1d1d33327776bc1dd3274c17cba7ce0ab824c
ajswago <ajswago@gmail.com>
2022-12-13 15:34:27 -0500

    update Gradle

--- app/src/main/java/com/swago/seenthemlive/ui/map/MapFragment.kt
+++ app/src/main/java/com/swago/seenthemlive/ui/map/MapFragment.kt
@@ -6,13 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.swago.seenthemlive.R
 
-class MapFragment : Fragment() {
+
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mapViewModel: MapViewModel
+
+    var mapFragment: SupportMapFragment? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +29,16 @@ class MapFragment : Fragment() {
         mapViewModel =
             ViewModelProvider(this).get(MapViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_map, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
-        mapViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        val fragmentManager = childFragmentManager
+        val mapFragment = fragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
         return root
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        // stuff
     }
 }
